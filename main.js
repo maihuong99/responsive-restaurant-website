fef0b83abf5be82288f9fe2166f40bb145564f52
maihuong99 <62054925+maihuong99@users.noreply.github.com>
2021-08-06 08:42:57 +0700

    complete search section and home section

--- main.js
+++ main.js
@@ -5,5 +5,31 @@ bars.onclick = () =>{
     bars.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+window.onscroll = () =>{
+    bars.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search-bar').onclick =()=>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick =()=>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlider: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction:false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    loop: true,
+  });
     
 
