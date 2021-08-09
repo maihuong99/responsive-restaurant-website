e234b6bb15ae9bbea5245e44866e86c17f607c42
maihuong99 <62054925+maihuong99@users.noreply.github.com>
2021-08-09 10:48:26 +0700

    complete scroll spy and loader part

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var bars = document.querySelector("#bars");
 var navbar = document.querySelector(".navbar");
+var links = document.querySelectorAll('header .navbar a');
+var section = document.querySelectorAll('section');
 
 bars.onclick = () =>{
     bars.classList.toggle('fa-times');
@@ -9,7 +11,36 @@ bars.onclick = () =>{
 window.onscroll = () =>{
     bars.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop -150;
+        let id= sec.getAttribute('id');
+     
+        if(top >= offset && top < offset + height){
+          
+            links.forEach(link =>{
+                
+                link.classList.remove('active');
+            })
+            document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+
+
+        }
+    })
 }
+
+links.forEach(link => {
+    link.onclick = () => {
+        var parent = link.parentElement;
+        for(let i=0; i<parent.children.length; i++){
+            parent.children[i].classList.remove('active')
+        }
+        link.classList.add('active');
+    }
+});
+
 
 document.querySelector('#search-bar').onclick =()=>{
     document.querySelector('#search-form').classList.toggle('active');
@@ -60,5 +91,16 @@ var swiper = new Swiper(".home-slider", {
 
     }
   });
+
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+
+function fadeOut(){
+    setInterval(loader, 2000);
+}
+
+window.onload = fadeOut;
     
 
